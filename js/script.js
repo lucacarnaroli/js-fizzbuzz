1e95dd4e94a66b72ce3a0252e227d35b747e9e1b
luca <luca.carnaroli98@gmail.com>
2020-01-09 18:04:01 +0100

    html

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 // Scrivi un programma che stampi i numeri da 1
-// a 100,
+// a 100,// ma per i multipli di 3 stampi “Fizz” al
+// posto del numero // e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3
+// che di 5 stampi FizzBuzz.
 
 for (var i = 1; i <= 100; i++) {
   if (i % 3==0 && i % 5 ==0) {
     console.log('fizzbuzz')
+    document.getElementById('lista').innerHTML += '<li>' + 'fizzbuzz'+'</li>';
   } else if (i % 3 == 0) {
     console.log('fizz');
+    document.getElementById('lista').innerHTML += '<li>'  + 'fizz'+'</li>';
   } else if (i % 5 == 0) {
     console.log('buzz');
+    document.getElementById('lista').innerHTML += '<li>' + 'buzz'+'</li>';
   } else {
     console.log(i);
+    document.getElementById('lista').innerHTML += '<li>' + i +'</li>';
    }
 }
-// ma per i multipli di 3 stampi “Fizz” al
-// posto del numero
-
-
-// e per i multipli di 5 stampi Buzz.
-// Per i numeri che sono sia multipli di 3
-// che di 5 stampi FizzBuzz.
