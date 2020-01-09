c14aad407209bfc51f8e666678694ab45b466ea3
luca <luca.carnaroli98@gmail.com>
2020-01-09 16:33:52 +0100

    fizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
 // Scrivi un programma che stampi i numeri da 1
 // a 100,
-var numeri = Math.floor(Math.random() * 101);
-for (var i = 0; i < 100; i++) {
-  console.log(numeri);
-  if (numeri / 3) {
-    console.log('fizz');
-  } else if (numeri / 5) {
-    console.log('buzz')
-  } else if (numeri / 3 && 5) {
-    console.log('fizz'||'buzz')
-  } else {
-    console.log(numeri);
+
+for (var i = 1; i <= 100; i++) {
+  if (i / 3 && i / 5) {
+    console.log('fizzbuzz')
   }
 
 }
 // ma per i multipli di 3 stampi “Fizz” al
 // posto del numero
-var tre = numeri / 3;
-var cinque = numeri / 5;
+
+
+// e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3
+// che di 5 stampi FizzBuzz.
